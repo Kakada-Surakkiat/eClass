8e16f3a43d3ca5c137df9d7c064f5cd37e6291ef
leapkh <leapkh@yahoo.com>
2016-12-13 19:59:59 +0700

    Facebook login

---
+++ app/src/main/java/kh/edu/rupp/ckcc/eclass/activity/LoginActivity.java
@@ -0,0 +1,75 @@
+package kh.edu.rupp.ckcc.eclass.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import kh.edu.rupp.ckcc.eclass.R;
+
+/**
+ * eClass
+ * Created by leapkh on 13/12/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(this);
+
+        if(AccessToken.getCurrentAccessToken() == null){
+            setContentView(R.layout.activity_login);
+            registerFacebookCallback();
+        }else{
+            startMainActivity();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void registerFacebookCallback(){
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startMainActivity();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/kh/edu/rupp/ckcc/eclass/view/NavHeaderView.java
@@ -0,0 +1,88 @@
+package kh.edu.rupp.ckcc.eclass.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.DrawableRes;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import kh.edu.rupp.ckcc.eclass.R;
+
+/**
+ * eClass
+ * Created by leapkh on 8/12/16.
+ */
+
+public class NavHeaderView extends RelativeLayout implements View.OnClickListener {
+
+    private CircleImageView imgProfile;
+    private TextView txtUsername;
+    private OnNavHeaderItemClick onNavHeaderItemClick;
+
+    public NavHeaderView(Context context) {
+        super(context);
+
+        initViews(context);
+    }
+
+    public NavHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        initViews(context);
+    }
+
+    public NavHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        initViews(context);
+    }
+
+    public void setOnNavHeaderItemClick(OnNavHeaderItemClick onNavHeaderItemClick) {
+        this.onNavHeaderItemClick = onNavHeaderItemClick;
+    }
+
+    private void initViews(Context context) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.nav_header, this);
+        imgProfile = (CircleImageView) findViewById(R.id.img_profile);
+        txtUsername = (TextView) findViewById(R.id.txt_username);
+        findViewById(R.id.txt_edit).setOnClickListener(this);
+        findViewById(R.id.txt_logout).setOnClickListener(this);
+    }
+
+    public void setUsername(String username) {
+        txtUsername.setText(username);
+    }
+
+    public void setImage(@DrawableRes int imageResId) {
+        imgProfile.setImageResource(imageResId);
+    }
+
+    public void setImage(Bitmap bitmap){
+        imgProfile.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.txt_edit) {
+            if (onNavHeaderItemClick != null) {
+                onNavHeaderItemClick.onEditClick();
+            }
+        } else if (v.getId() == R.id.txt_logout) {
+            if (onNavHeaderItemClick != null) {
+                onNavHeaderItemClick.onLogoutClick();
+            }
+        }
+    }
+
+    public interface OnNavHeaderItemClick {
+        void onEditClick();
+
+        void onLogoutClick();
+    }
+
+}
